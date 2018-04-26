6ffad564d15b417c9d0edb8a2d4d6645f80ee09c
JigyasaTomar <jigs.kcs@gmail.com>
2018-04-27 02:03:54 +0530

    adding blinker in registration form

---
+++ src/main/webapp/js/commonFunctions.js
@@ -0,0 +1,7 @@
+// Blinkers in subscribe, find-tutor and become-tutor
+ function blinker()
+		 {
+		   $('.blinking').fadeOut(800);
+		  $('.blinking').fadeIn(800); 
+		 }
+		 setInterval(blinker, 2100);
