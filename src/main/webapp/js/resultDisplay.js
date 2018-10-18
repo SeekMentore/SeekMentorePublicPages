b7a395cc89ad0fa3160eab75cb37ac040f6713fe
JigyasaTomar <jigs.kcs@gmail.com>
2018-10-19 02:39:43 +0530

    Students Result Page

--- src/main/webapp/js/resultDisplay.js
+++ src/main/webapp/js/resultDisplay.js
@@ -1,63 +1,76 @@
 var studentJSON = "[ {\"firstName\":\"Shantanu\", \"lastName\":\"Mukherjee\", \"className\":\"VII\", \"phoneNo\":\"1234567890\", \"marks\":\"3\", \"learningStyle\":\"sdgashdgsahdsd\", \"suggestions\":\"dfdfdsfds\" },{\"firstName\":\"Jigyasa\", \"lastName\":\"Mukherjee\", \"className\":\"VII\", \"phoneNo\":\"1234567890\", \"marks\":\"3\", \"learningStyle\":\"sdgashdgsahdsd\", \"suggestions\":\"dfdfdsfds\" },	{\"firstName\":\"Silky\", \"lastName\":\"Mukherjee\", \"className\":\"VII\", \"phoneNo\":\"1234567890\", \"marks\":\"3\", \"learningStyle\":\"sdgashdgsahdsd\", \"suggestions\":\"dfdfdsfds\" } ]";
 
-
 function submitFormResult() {	
 	
 	var JSON_Object = JSON.parse(studentJSON);
-	var i;
 	var innerHTML='';
-	var firstName = document.getElementById('firstname');
-	var lastName = document.getElementById('lastname');
-	var classNo = document.getElementById('student-grade');
-	var contact = document.getElementById('contact-number');
-    var studentMatch = false;  
-/*	var firstName = 'Shantanu';
-	var lastName = 'Shntanu';
-	var classNo = 'VII';
-	var contact = 1234567890;*/
+	var firstName = $('#firstname');
+	var lastName = $('#lastname');
+	var classNo = $('#student-grade-result');
+	var contact = $('#contact-number');
+	var dataValidated = true;  
 	
-    /*if(firstName.match(/^[A-Za-z]+$/) == false)
+
+    if(!firstName.val().match(/^[A-Za-z]+$/))
     {
-    	alert("First Name must contains only Letters.")
+    	$('#display-result').html('Please enter valid Details.').css('color', 'red');
+        firstName.focus();
+        dataValidated=false;
     }
-    if(lastName.match(/^[A-Za-z]+$/) == false)
+    if(!lastName.val().match(/^[A-Za-z]+$/))
     {
-    	document.getElementById('display-result').innerHTML = 'Last Name must contains only Letters.'
-    }
-    if(contact.match(/^\d{10}$/) == false)
+    	$('#display-result').html('Please enter valid Details.').css('color', 'red');
+        lastName.focus();
+        dataValidated=false;
+    }    
+    if(!contact.val().match(/^[0-9]+$/))
     {
-    	document.getElementById('display-result').innerHTML = 'Please enter correct Contact No.'
+    	$('#display-result').html('Please enter valid Details.').css('color', 'red');
+    	contact.focus();
+    	dataValidated=false;
     }
-    if(classNo != 'VII' && classNo != 'VIII' && classNo != 'IX' )
+    if(firstName.val()==""||contact.val()==""|| lastName.val()==""||classNo.val()=="")
     {
-    	document.getElementById('display-result').innerHTML = 'Please enter correct Standard.'
+    	$('#display-result').html('One or more fields are empty. Please enter all the details.').css('color', 'red');
+    	dataValidated=false;
     }
-*/
-	for(i=0; i<JSON_Object.length; i++)
-	{
-		if(firstName == JSON_Object[i].firstName && classNo == JSON_Object[i].className && contact ==  JSON_Object[i].phoneNo  )
-			{
-			  innerHTML += '<tr ><td> Name </td><td>'+ '<input type="text" style="font-family: cursive" value ="'+JSON_Object[i].firstName +'  '+ JSON_Object[i].lastName + '"disabled> </td></tr>';
-			  innerHTML += '<tr><td> Gender </td><td>'+ '<input type="text" style="font-family: cursive" value ="'+ JSON_Object[i].className +'"disabled></td></tr>';
-			  innerHTML += '<tr><td> Class </td><td>'+ '<input type="text" style="font-family: cursive" value ="'+ JSON_Object[i].className +'"disabled></td></tr>';
-			  innerHTML += '<tr><td> Marks </td><td>'+ '<input type="text"  style="font-family: cursive" value ="'+ JSON_Object[i].marks +'"disabled></td></tr>';
-			  innerHTML += '<tr><td> Learning Style </td><td>'+ '<input type="text" style="font-family: cursive" value ="' + JSON_Object[i].learningStyle + '"disabled></td></tr>';
-			  innerHTML += '<tr><td> Suggestions </td><td>'+ '<textarea  style="font-family: cursive  overflow: scroll "rows="2" cols="15"  readonly>'+ JSON_Object[i].suggestions + '</textarea>' +'</td></tr>';
-			  studentMatch = true;
-			}
-		
-	}
-	if(studentMatch)
-	{   $('#details-form').addClass('noscreen');
-		$('#display-result').html(innerHTML);
-	}
-	else
-	{
-		alert("Please check your details.One or more details entered is incorrect!!")
-	}
-	
-	
+
+    if(dataValidated)
+    {
+
+    	for(i=0; i<JSON_Object.length; i++)
+    	{
+
+    		if(firstName.value == JSON_Object[i].firstName && classNo.value == JSON_Object[i].className && contact.value ==  JSON_Object[i].phoneNo  )
+    		{
+    			$('#display-result').html('');
+    			innerHTML += '<tr ><td> Name </td><td>'+ '<input type="text" style="font-family: cursive" value ="'+JSON_Object[i].firstName +'  '+ JSON_Object[i].lastName + '"disabled> </td></tr>';
+    			innerHTML += '<tr><td> Class </td><td>'+ '<input type="text" style="font-family: cursive" value ="'+ JSON_Object[i].className +'"disabled></td></tr>';
+    			innerHTML += '<tr><td> Marks </td><td>'+ '<input type="text"  style="font-family: cursive" value ="'+ JSON_Object[i].marks +'"disabled></td></tr>';
+    			innerHTML += '<tr><td> Learning Style </td><td>'+ '<input type="text" style="font-family: cursive" value ="' + JSON_Object[i].learningStyle + '"disabled></td></tr>';
+    			innerHTML += '<tr><td> Suggestions </td><td>'+ '<textarea  style="font-family: cursive  overflow: scroll "rows="2" cols="15"  readonly>'+ JSON_Object[i].suggestions + '</textarea>' +'</td></tr>';
+
+    			$('#details-form').addClass('noscreen');
+    			$('#display-result').html(innerHTML);
+    		}
+
+    		else
+    		{
+    			$('#display-result').html('Please check your details.One or more details entered is incorrect!!');
+    		}
+    	}
+
+    } 
 }
+    
+    function resetForm(resetBtnId) {
+    	var resetBtn = document.getElementById(resetBtnId);
+    	if (null !=  resetBtn) {
+    		resetBtn.click()
+    	}
+    	$('#display-result').html('');
+    }
+
 	
 
 
